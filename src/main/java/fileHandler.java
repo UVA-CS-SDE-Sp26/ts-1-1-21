e9d21f9df93f3606f5a780c5c99672e149e558da
Dimash <Dimash.Adikhan@gmail.com>
2026-02-12 15:27:01 -0500

    Committing data folder for filehandler.

--- src/main/java/fileHandler.java
+++ src/main/java/fileHandler.java
@@ -1,49 +1,80 @@
 /**
- * @author Dimash Adikhan
+ * @author Dimash Adikhan jyv8md
  * 2/9/2026
  */
 
 import java.util.*;
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+
+//Citing my code:https://www.w3schools.com/java/java_files_read.asp
+//https://docs.oracle.com/javase/8/docs/api/java/io/BufferedReader.html
 
 
+public class fileHandler implements fileHandlerInterface{
+    private Map<String, String> data = new LinkedHashMap<>();
 
-public class fileHandler {
-    private Map<String, String> data = new HashMap<>();
+    public fileHandler(){
+        data = new HashMap<>();
+        this.loadFiles();
+    }
 
-    public String readFiles() {
-        File folder = new File("data"); //data file directory
+
+    private void loadFiles(){ //loads files
+        File folder = new File("data");
+
         if (!folder.exists() || !folder.isDirectory()) {
-            return "Data folder is not found in TopSecret";
+            System.out.println("Error: data folder is not found in TopSecret program" + System.getProperty("user.dir"));
+            return;
         }
-
-        StringBuilder str = new StringBuilder("Reading: ");
         File[] files = folder.listFiles();
         int count = 0;
 
         if (files != null) {
+            Arrays.sort(files);
             for (File file : files) {
-                if (file.isFile()) {
-                    String formatted = String.format("%02d", count); //
-                    data.put(formatted, file.getName()); //adds files into data map.
-                    str.append(file.getName()).append(" ");
+                if (file.isFile()&&!file.getName().startsWith(".")) {
+                    String formatted = String.format("%02d", count);
+                    data.put(formatted, file.getName());
                     count++;
                 }
             }
         }
-        return str.toString();
-    }// Returns every file in the data folder
-
-    public String getFileName(){
-        return "";
-    }// Returns the file name of certain files in data directory
-
-    public String getUserInput(){
-
     }
+    @Override
+    public String readFiles(){ //Reads all files in the hash (By insert order)
 
-    public String readFileData(int UserIn){ //Reads in the data
-        return "";
+        String str = "";
+        for(Map.Entry<String, String> element: data.entrySet()){
+            String key = element.getKey();
+            String val = element.getValue();
+            str += key + " " + val + "\n";
+        }
+        return str;
+    }// Returns every file name in the data folder
+
+    @Override
+    public String readFileData(String userIn){ //Reads in data by ID (STARTS FROM 00)
+        String fileName = data.get(userIn);
+        if (fileName==null) {
+            return "Invalid file number.";
+        }
+
+        File file = new File("data", fileName);
+        String content = "";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(file))){
+            String line;
+            while((line=br.readLine()) != null){
+                content+=(line)+"\n";
+            }
+        }
+        catch (IOException e){
+            return "Error reading the file.";
+        }
+        return content;
     }
 
 }
--- src/main/java/fileHandlerInterface.java
+++ src/main/java/fileHandlerInterface.java
@@ -1,2 +1,19 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Dimash Adikhan jyv8md
+ * 2/9/2026
+ */
 public interface fileHandlerInterface {
+
+
+    String readFiles();
+
+    String readFileData(String userIn);//Reads in the data
+
+
+
 }
