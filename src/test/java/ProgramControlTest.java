9c2cc62f5c2476d87a764dd5578204b0abfcd40d
rormcg <rorymcgui@gmail.com>
2026-02-13 15:37:52 -0500

    Finalize (?) ProgramControl Tests

--- src/test/java/ProgramControlTest.java
+++ src/test/java/ProgramControlTest.java
@@ -79,9 +79,12 @@ class ProgramControlTest {
     ////Testing requestFile()////
     /* Cases:
     * empty args - returns listFiles()
-    * 1 args - return ciphered contents
-    * 2 args - return deciphered contents
-    * >2 args -
+    * 1 args (valid) - return ciphered contents
+        * invalid arg (<1, not an int, empty) - IllegalArgument Exception
+    * 2 args (valid) - return deciphered contents
+        * first arg invalid (<1, not an int, empty) - IllegalArgumentException
+        * second arg invalid (empty String) - return ciphered contents
+    * >2 args - IllegalArgumentException
      */
 
     //requestFile called with no args
@@ -92,32 +95,229 @@ class ProgramControlTest {
                 "02 file2.txt\n" +
                 "03 file3.txt";
 
-        mockControl = new ProgramControl(mockHandler);
-        when(mockControl.listFiles()).thenReturn(fileList);
+        ProgramControl control = new ProgramControl(mockHandler);
+        when(mockHandler.readFiles()).thenReturn(fileList);
 
 
-        assertEquals(fileList, mockControl.requestFile(args),
-                "requestFile() failed to return listArgs() when no args were provided");
-        //assertEquals(fileList + System.lineSeparator(), outputStream.toString(),
-        //        "requestFile() did not print file list as intended");
+        assertEquals(fileList, control.requestFile(args),
+                "requestFile() failed to return the file list when no args were provided");
     }
 
-    //valid arg is passed
+    //1 arg is passed
     @Test
-    void testRequestFileWithInput() {
-        String fileContents = "File Contents!";
+    void testRequestFileArg1Valid() {
+        String fileContentsCiphered = "ciphertextxyz" ;
+        String fileContentsDeciphered = "File Contents!";
         String[] args = {"1"};
         String fileList = "01 file1.txt\n" +
                 "02 file2.txt\n" +
                 "03 file3.txt";
 
         ProgramControl control = new ProgramControl(mockHandler);
-        //when(handler.readFileData()).thenReturn(fileContents);
+        //UiRequest request = new UiRequest(args[1], args[2])
+        //Cannot mock UserInterface.parseArgs because it is static (?)
+        when(mockHandler.readFileData(any(UiRequest.class))).thenReturn(fileContentsCiphered);
 
-        //when(handler.readFiles()).thenReturn(fileList);
-        //control.listFiles();
-        //assertEquals(fileList + System.lineSeparator(), outputStream.toString(),
-        //        "listFiles() printed incorrect value to out");
-        assertEquals(fileContents, control.requestFile(args), "");
+        assertEquals(fileContentsCiphered, control.requestFile(args),
+                "when 1 valid arg was passed, requestFile should have returned the ciphered text");
+    }
+
+    //2 valid args are passed
+    @Test
+    void testRequestFileArg1ValidArg2Valid() {
+        String fileContentsCiphered = "ciphertextxyz" ;
+        String fileContentsDeciphered = "File Contents!";
+        String[] args = {"1", "decipher-key"};
+        String fileList = "01 file1.txt\n" +
+                "02 file2.txt\n" +
+                "03 file3.txt";
+
+        ProgramControl control = new ProgramControl(mockHandler);
+
+        //Cannot mock UserInterface.parseArgs because it is static (?)
+        when(mockHandler.readFileData(any(UiRequest.class))).thenReturn(fileContentsDeciphered);
+
+        assertEquals(fileContentsDeciphered, control.requestFile(args),
+                "when 1 valid arg was passed, requestFile should have returned the ciphered text");
+    }
+
+    //1 invalid non-int arg is passed
+    @Test
+    void testRequestFileArg1NotInt() {
+        String fileContentsCiphered = "ciphertextxyz" ;
+        String fileContentsDeciphered = "File Contents!";
+        String[] args = {"not an integer"};
+        String fileList = "01 file1.txt\n" +
+                "02 file2.txt\n" +
+                "03 file3.txt";
+
+        ProgramControl control = new ProgramControl(mockHandler);
+        //UiRequest request = new UiRequest(args[1], args[2])
+        //Cannot mock UserInterface.parseArgs because it is static (?)
+        when(mockHandler.readFileData(any(UiRequest.class))).thenReturn(fileContentsCiphered);
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> control.requestFile(args)
+        );
+        assertEquals("File ID must be a number", exception.getMessage(),
+                "requestFile did not throw correct message when one non-int arg was passed to it");
+    }
+
+    //1 invalid arg <1 is passed
+    @Test
+    void testRequestFileArg1LessThanOne() {
+        String fileContentsCiphered = "ciphertextxyz" ;
+        String fileContentsDeciphered = "File Contents!";
+        String[] args = {"0"};
+        String fileList = "01 file1.txt\n" +
+                "02 file2.txt\n" +
+                "03 file3.txt";
+
+        ProgramControl control = new ProgramControl(mockHandler);
+        //UiRequest request = new UiRequest(args[1], args[2])
+        //Cannot mock UserInterface.parseArgs because it is static (?)
+        when(mockHandler.readFileData(any(UiRequest.class))).thenReturn(fileContentsCiphered);
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> control.requestFile(args)
+        );
+        assertEquals("Invalid File ID (must be greater than 0)", exception.getMessage(),
+                "requestFile did not throw correct message when one arg < 1 was passed to it");
+    }
+
+    //1 invalid arg <1 is passed
+    @Test
+    void testRequestFileArg1Empty() {
+        String fileContentsCiphered = "ciphertextxyz" ;
+        String fileContentsDeciphered = "File Contents!";
+        String[] args = {""};
+        String fileList = "01 file1.txt\n" +
+                "02 file2.txt\n" +
+                "03 file3.txt";
+
+        ProgramControl control = new ProgramControl(mockHandler);
+        //UiRequest request = new UiRequest(args[1], args[2])
+        //Cannot mock UserInterface.parseArgs because it is static (?)
+        when(mockHandler.readFileData(any(UiRequest.class))).thenReturn(fileContentsCiphered);
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> control.requestFile(args)
+        );
+        assertEquals("File ID cannot be empty", exception.getMessage(),
+                "requestFile did not throw correct message when one empty arg was passed to it");
+    }
+
+    //1 valid arg, 2nd invalid arg is passed
+    @Test
+    void testRequestFileArg1ValidArg2Invalid() {
+        String fileContentsCiphered = "ciphertextxyz" ;
+        String fileContentsDeciphered = "File Contents!";
+        String[] args = {"1", null};
+        String fileList = "01 file1.txt\n" +
+                "02 file2.txt\n" +
+                "03 file3.txt";
+
+        ProgramControl control = new ProgramControl(mockHandler);
+        //UiRequest request = new UiRequest(args[1], args[2])
+        //Cannot mock UserInterface.parseArgs because it is static (?)
+        when(mockHandler.readFileData(any(UiRequest.class))).thenReturn(fileContentsCiphered);
+
+        assertEquals(fileContentsCiphered, control.requestFile(args),
+                "when 1 valid arg was passed with an invalid second arg, requestFile should have returned the ciphered text");
+    }
+
+    //1 invalid arg <1 is passed
+    @Test
+    void testRequestFileArg1LessThanOneArg2Valid() {
+        String fileContentsCiphered = "ciphertextxyz" ;
+        String fileContentsDeciphered = "File Contents!";
+        String[] args = {"0", "valid-cipher-key"};
+        String fileList = "01 file1.txt\n" +
+                "02 file2.txt\n" +
+                "03 file3.txt";
+
+        ProgramControl control = new ProgramControl(mockHandler);
+        //UiRequest request = new UiRequest(args[1], args[2])
+        //Cannot mock UserInterface.parseArgs because it is static (?)
+        when(mockHandler.readFileData(any(UiRequest.class))).thenReturn(fileContentsCiphered);
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> control.requestFile(args)
+        );
+        assertEquals("Invalid File ID (must be greater than 0)", exception.getMessage(),
+                "requestFile did not throw correct message when one arg < 1 and a second valid arg was passed to it");
+    }
+
+    //1 invalid arg <1 is passed
+    @Test
+    void testRequestFileArg1NotIntArg2Valid() {
+        String fileContentsCiphered = "ciphertextxyz" ;
+        String fileContentsDeciphered = "File Contents!";
+        String[] args = {"not an integer", "valid-cipher-key"};
+        String fileList = "01 file1.txt\n" +
+                "02 file2.txt\n" +
+                "03 file3.txt";
+
+        ProgramControl control = new ProgramControl(mockHandler);
+        //UiRequest request = new UiRequest(args[1], args[2])
+        //Cannot mock UserInterface.parseArgs because it is static (?)
+        when(mockHandler.readFileData(any(UiRequest.class))).thenReturn(fileContentsCiphered);
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> control.requestFile(args)
+        );
+        assertEquals("File ID must be a number", exception.getMessage(),
+                "requestFile did not throw correct message when one arg was not an int and a second valid arg was passed to it");
+    }
+
+    //1 invalid arg <1 is passed
+    @Test
+    void testRequestFileArg1EmptyArg2Valid() {
+        String fileContentsCiphered = "ciphertextxyz" ;
+        String fileContentsDeciphered = "File Contents!";
+        String[] args = {"", "valid-cipher-key"};
+        String fileList = "01 file1.txt\n" +
+                "02 file2.txt\n" +
+                "03 file3.txt";
+
+        ProgramControl control = new ProgramControl(mockHandler);
+        //UiRequest request = new UiRequest(args[1], args[2])
+        //Cannot mock UserInterface.parseArgs because it is static (?)
+        when(mockHandler.readFileData(any(UiRequest.class))).thenReturn(fileContentsCiphered);
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> control.requestFile(args)
+        );
+        assertEquals("File ID cannot be empty", exception.getMessage(),
+                "requestFile did not throw correct message when one arg was empty and a second valid arg was passed to it");
+    }
+
+    //1 invalid arg <1 is passed
+    @Test
+    void testRequestFileOverTwoArgs() {
+        String fileContentsCiphered = "ciphertextxyz" ;
+        String fileContentsDeciphered = "File Contents!";
+        String[] args = {"1", "valid-cipher-key", "2"};
+        String fileList = "01 file1.txt\n" +
+                "02 file2.txt\n" +
+                "03 file3.txt";
+
+        ProgramControl control = new ProgramControl(mockHandler);
+        //UiRequest request = new UiRequest(args[1], args[2])
+        //Cannot mock UserInterface.parseArgs because it is static (?)
+        when(mockHandler.readFileData(any(UiRequest.class))).thenReturn(fileContentsCiphered);
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> control.requestFile(args)
+        );
+        assertEquals("Too many command line arguments.", exception.getMessage(),
+                "requestFile did not throw correct message 3 args were passed to it");
     }
 }
